02cd9bc8a83dd45a2bea471cfbfdc9888aa0a1bf
Aleksandr Tulmentyev <lexitor34@gmail.com>
2021-08-10 16:57:09 +0300

    Add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :web do
+
+    namespace :admin do
+      resources :admins, only: %i[index new create]
+    end
+  end
 end
