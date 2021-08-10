7e04f3c497500ad267ccd6386d3a88943b4613f7
Aleksandr <lexitor34@gmail.com>
2021-08-10 12:58:16 +0300

    Add test for admin model

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should create admin' do
+    admin = build(:admin)
+    admin.save
+
+    created_admin = Admin.last
+    assert_equal(admin.email, created_admin.email)
+  end
+
+  test 'Should not create admin without email' do
+    admin = build :admin, email: nil
+    admin.save
+
+    created_admin = Admin.last
+    assert_nil created_admin
+  end
+
+  test 'email should be unique' do
+    first_admin = build :admin, email: 'test@gmail.com'
+    first_admin.save
+
+    second_admin = build :admin, email: 'test@gmail.com'
+    assert_not second_admin.save
+  end
+
+  test 'password should be longer than 6 characters' do
+    admin = build :admin, password: '1234'
+    admin.save
+
+    created_admin = Admin.last
+    assert_nil created_admin
+  end
 end
