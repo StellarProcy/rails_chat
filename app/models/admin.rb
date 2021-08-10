21cc3476d4d67cf472eaa41b4e28b0cd9c35523e
Aleksandr <lexitor34@gmail.com>
2021-08-10 12:08:33 +0300

    Add states and validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,19 @@
 class Admin < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence:true, uniqueness:true, email: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  state_machine initial: :active do
+    state :active
+    state :deleted
+
+    event :del do
+      transition from: :active, to: :deleted, if: :active?
+    end
+
+    event :activate do
+      transition from: :deleted, to: :active, if: :deleted?
+    end
+  end
 end
