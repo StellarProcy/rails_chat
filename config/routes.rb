ef7e5f3fda3c26cfff0bdb3e64e316a3df29b1d9
Aleksandr Tulmentyev <lexitor34@gmail.com>
2021-08-11 12:25:05 +0300

    Add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   scope module: :web do
 
     namespace :admin do
+      root to: 'admins#index'
+
       resources :admins, only: %i[index new create]
     end
   end
