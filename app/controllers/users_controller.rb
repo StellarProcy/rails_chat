41327ba50d75ab32e9b6b22fcec498c9e637c894
Igor Yukhno <procyu@gmail.com>
2021-08-11 13:03:08 +0400

    add state_machine for user, add some methods for user controller, some fixes in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       flash[:notice] = 'Signup successful'
     else
       flash[:notice] = 'Please try again'
       redirect_to '/signup'
     end
   end
-
+  
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 
   validates :login, presence: true, uniqueness: { case_sensitive: true }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
-  validates :firstname, :password, presence: true
+  validates :firstname, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
