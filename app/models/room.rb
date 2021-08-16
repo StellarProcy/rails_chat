1ed0a91e4d76f7e7105b5c639c39062d38d60867
Igor Yukhno <procyu@gmail.com>
2021-08-16 16:40:32 +0400

    some changes in models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  has_many :users
+  has_many :messages
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :rooms
+  has_many :messages
 
   validates :login, presence: true, uniqueness: { case_sensitive: true }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
