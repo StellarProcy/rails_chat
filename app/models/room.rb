2083af8c4d6dd261b31283b29fb45fbbe1f996e8
Igor Yukhno <procyu@gmail.com>
2021-08-16 17:53:46 +0400

    add some relations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :users
-  has_many :room_messages
+  has_many :room_messages, dependent: :destroy, inverse_of: :room
   has_many :messages, through: :room_messages
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
